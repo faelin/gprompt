f755f6018f5625898904049c9db3d09fcfc94619
Faelin <faelin.landy@gmail.com>
2019-04-12 18:15:19 -0400

    added proper escapes to the color-coding so that the prompt will wrap text appropriately.

--- bash/.git_prompt.sh
+++ bash/.git_prompt.sh
@@ -1,31 +1,31 @@
-COLOR_RED='\033[91m'
-COLOR_GREEN='\033[92m'
-COLOR_YELLOW='\033[93m'
-COLOR_PINK='\033[95m'
-COLOR_CYAN='\033[96m'
-COLOR_RESET='\033[0m'
-STYLE_BOLD='\033[1m'
-STYLE_RESET='\033[22m'
+COLOR_RED='\\033[91m'
+COLOR_GREEN='\\033[92m'
+COLOR_YELLOW='\\033[93m'
+COLOR_PINK='\\033[95m'
+COLOR_CYAN='\\033[96m'
+COLOR_RESET='\\033[0m'
+STYLE_BOLD='\\033[1m'
+STYLE_RESET='\\033[22m'
 
 
 parse_git_branch() {
-	staged_count=$(git status --no-optional-locks --porcelain=1 2> /dev/null | grep -e '^[^?! ]' | wc -l | sed -E "s/([0-9]+``)/<\1>/; s/\<0\>//" | xargs)
-	unstag_count=$(git status --no-optional-locks --porcelain=1 2> /dev/null | grep -e '^.[^ ]'  | wc -l | sed -E "s/([0-9]+)/(\1)/; s/\(0\)//" | xargs)
+	staged_count=$(git status --porcelain=1 2> /dev/null | grep -e '^[^?! ]' | wc -l | sed -E "s/([0-9]+``)/<\1>/; s/\<0\>//" | xargs)
+	unstag_count=$(git status --porcelain=1 2> /dev/null | grep -e '^.[^ ]'  | wc -l | sed -E "s/([0-9]+)/(\1)/; s/\(0\)//" | xargs)
 	file_state=$([ ${unstag_count} ] || [ ${staged_count} ] && echo ' '${staged_count}${unstag_count})
 	git_state=$(git branch -v 2> /dev/null | grep '^* ' | sed -E "s/^\* ([^ ]+) [A-Za-z0-9]+( \[(behind|ahead) ([0-9]+)\])? .*$/[\1\2$file_state]/")
 	git_state=$(echo $git_state | sed -E "s/ \[behind ([0-9]+)\]/~\1/; s/ \[ahead ([0-9]+)\]/+\1/")
 	echo $git_state
 }
-print_git_branch() {
-	parse_git_branch | sed -E "s/^\[([^~+ ]+)(~[0-9]+)?(\+[0-9]+)?( )?(<[0-9]+>)?(\([0-9]+\))?\]$/$(echo -e " ${STYLE_BOLD}${COLOR_YELLOW}[\1${COLOR_RED}\2${COLOR_GREEN}\3\4${COLOR_CYAN}\5${COLOR_PINK}\6${COLOR_YELLOW}]${COLOR_RESET}")/"
+color_git_branch() {
+	parse_git_branch | sed -E "s/^\[([^~+ ]+)(~[0-9]+)?(\+[0-9]+)?( )?(<[0-9]+>)?(\([0-9]+\))?\]$/$(echo " \\\[${STYLE_BOLD}${COLOR_YELLOW}\\\][\1\\\[${COLOR_RED}\\\]\2\\\[${COLOR_GREEN}\\\]\3\4\\\[${COLOR_CYAN}\\\]\5\\\[${COLOR_PINK}\\\]\6\\\[${COLOR_YELLOW}\\\]]\\\[${COLOR_RESET}\\\]")/"
 }
  
 parse_git_branch_with_repo() {
 	repo_name=$(basename -s .git `git config --get remote.origin.url` 2> /dev/null)
 	parse_git_branch | sed -E "s/^\[(.*)\]$/[${repo_name}:\1]/"
 }
-print_git_branch_with_repo() {
-	parse_git_branch_with_repo | sed -E "s/^\[([^:]+)(:[^~+ ]+)(~[0-9]+)?(\+[0-9]+)?( )?(<[0-9]+>)?(\([0-9]+\))?\]$/$(echo -e " ${STYLE_BOLD}${COLOR_YELLOW}[${STYLE_RESET}\1${STYLE_BOLD}\2${COLOR_RED}\3${COLOR_GREEN}\4\5${COLOR_CYAN}\6${COLOR_PINK}\7${COLOR_YELLOW}]${COLOR_RESET}")/"
+color_git_branch_with_repo() {
+	parse_git_branch_with_repo | sed -E "s/^\[([^:]+)(:[^~+ ]+)(~[0-9]+)?(\+[0-9]+)?( )?(<[0-9]+>)?(\([0-9]+\))?\]$/$(echo " \\\[${STYLE_BOLD}${COLOR_YELLOW}\\\][\\\[${STYLE_RESET}\\\]\1\\\[${STYLE_BOLD}\\\]\2\\\[${COLOR_RED}\\\]\3\\\[${COLOR_GREEN}\\\]\4\5\\\[${COLOR_CYAN}\\\]\6\\\[${COLOR_PINK}\\\]\7\\\[${COLOR_YELLOW}\\\]]\\\[${COLOR_RESET}\\\]")/"
 }
  
 basename_or_pwd() {
@@ -51,23 +51,23 @@ basename_or_pwd() {
 #
 #
 ##   EITHER basename [git_state] OR pwd (if not in a repo) $>
-export PS1='\[\033[1;34m\]$(basename_or_pwd)\[\033[0m\]$(print_git_branch)\[\033[97m\] \$> \[\033[0m\]'
-#export PS1='\[\033[1;34m\]$(basename_or_pwd)\[\033[0m\]$(print_git_branch_with_repo)\[\033[97m\] \$> \[\033[0m\]'
+#export PS1="\[\033[1;34m\]$(basename_or_pwd)\[\033[0m\]$(color_git_branch)\[\033[97m\] \$> \[\033[0m\]"
+export PS1="\[\033[1;34m\]$(basename_or_pwd)\[\033[0m\]$(color_git_branch_with_repo)\[\033[97m\] \$> \[\033[0m\]"
 #
 # 
 ##   pwd [git_state] $>
-#export PS1='\[\033[1;34m\w\[\033[0m\]\[`print_git_branch`\]\[\033[97m\] \$> \[\033[0m\]'
-#export PS1='\[\033[1;34m\w\[\033[0m\]\[`print_git_branch_with_repo`\]\[\033[97m\] \$> \[\033[0m\]'
+#export PS1="\[\033[1;34m\]\w\[\033[0m\]$(color_git_branch)\[\033[97m\] \$> \[\033[0m\]"
+#export PS1="\[\033[1;34m\]\w\[\033[0m\]$(color_git_branch_with_repo)\[\033[97m\] \$> \[\033[0m\]"
 #
 #
 ##   user@basename [git_state] $>
-#export PS1='\[\u\]:\[\033[96m\W\[\033[0m\]\[`print_git_branch`\]\[\033[97m\] \$> \[\033[0m\]'
-#export PS1='\[\u\]:\[\033[96m\W\[\033[0m\]\[`print_git_branch_with_repo`\]\[\033[97m\] \$> \[\033[0m\]'
+#export PS1="\[\033[0m\]\u:\[\033[96m\]\W\[\033[0m\]$(color_git_branch)\[\033[97m\] \$> \[\033[0m\]"
+#export PS1="\[\033[0m\]\u:\[\033[96m\]\W\[\033[0m\]$(color_git_branch_with_repo)\[\033[97m\] \$> \[\033[0m\]"
 #
 #
 ##   user:pwd [git_state] $>
-#export PS1='\[\033[39m\u\]:\[\033[96m\w\[\033[0m\]\[`print_git_branch`\]\[\033[97m\] \$> \[\033[0m\]'
-#export PS1='\[\033[39m\u\]:\[\033[96m\w\[\033[0m\]\[`print_git_branch_with_repo`\]\[\033[97m\] \$> \[\033[0m\]'
+#export PS1="\[\033[39m\]\u:\[\033[96m\]\w\[\033[0m\]$(color_git_branch)\[\033[97m\] \$> \[\033[0m\]"
+#export PS1="\[\033[39m\]\u:\[\033[96m\]\w\[\033[0m\]$(color_git_branch_with_repo)\[\033[97m\] \$> \[\033[0m\]"
 #
 #
 ###
